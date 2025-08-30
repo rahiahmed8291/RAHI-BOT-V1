823bad75a8c795d70baa243347c88c0488b73ee4
RAHI BOT BBY <rahiahmed143@gmail.com>
2025-08-30 20:11:50 +0600

    Update allbots.js

--- plugins/allbots.js
+++ plugins/allbots.js
@@ -9,29 +9,17 @@ module.exports = {
     description: "Send all my bot links and tutorials",
     categories: "Utility",
     usages: [".allbots"],
-    credit: "Developed by Mohammad Nayan"
+    credit: "Developed by Mohammad Rahi"
   },
 
   start: async ({ api, event }) => {
     const { threadId, message } = event;
 
-    const myNumber = "8801615298449";
-    const msg = `🌟 *NAYAN BOT COLLECTION* 🌟
-
-📱 *WhatsApp Bot*
-Link: https://github.com/MOHAMMAD-NAYAN-07/NAYAN-WHATSAPP-BOT
-Tutorial: https://youtu.be/hrO_v3z-Lq8
-
-💬 *Messenger Bot*
-Link: https://github.com/MOHAMMAD-NAYAN-07/Nayan-Bot
-Tutorial: https://youtu.be/DrQw3j56Llk
-
-🤖 *Telegram Bot*
-Link: https://github.com/MOHAMMAD-NAYAN-07/NAYAN-TELEGRAM-BOT
-Tutorial: https://youtu.be/FyjUL6MwaXs
+    const myNumber = "8801711209381";
+    const msg = `🌟 *RAHI BOT COLLECTION* 🌟
 
 🛠️ *Support Channel*
-Link: https://chat.whatsapp.com/KuSent8x2rIIl93CD1aKBl?mode=ems_copy_c
+Link: https://chat.whatsapp.com/JzyhgrrhPvoI83EYQrJqsj?mode=ems_copy_t
 
 📞 *Contact Me*: +${myNumber}`;
 
