329f1e640ce250afe106dd25412180b5903ee950
RAHI BOT BBY <rahiahmed143@gmail.com>
2025-08-30 20:24:41 +0600

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -5,7 +5,7 @@ module.exports = {
     permission: 2,
     prefix: 'both',
     categories: 'Utilities',
-    credit: 'Developed by Mohammad Nayan',
+    credit: 'Developed by Mohammad Rahi',
     usages: [
       `${global.config.PREFIX}contact - Send contact information for Mohammad Nayan.`,
     ]
@@ -16,16 +16,16 @@ module.exports = {
 
     const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
-            + 'FN:Mohammad Nayan\n'
+            + 'FN:Mohammad Rahi\n'
             + 'ORG:Nayan;\n'
-            + 'TEL;type=CELL;type=VOICE;waid=8801615298449:01615298449\n'
+            + 'TEL;type=CELL;type=VOICE;waid=8801711209381:01711209381\n'
             + 'END:VCARD';
 
     const sentMsg = await api.sendMessage(
       threadId,
       { 
         contacts: { 
-          displayName: 'Mohammad Nayan', 
+          displayName: 'Mohammad Rahi', 
           contacts: [{ vcard }] 
         }
       }
